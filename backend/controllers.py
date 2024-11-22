4dbd160f9cb0e19cb4685975ce2a6df3f3f6b9e6
Kaamesh2002 <kaameshkumaravel2002@gmail.com>
2024-11-23 02:04:50 +0530

    Adding Venue by Admin

--- backend/controllers.py
+++ backend/controllers.py
@@ -1,5 +1,5 @@
 # app routes
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,url_for,redirect
 from .models import *
 from flask import current_app as app
 
@@ -14,9 +14,9 @@ def signin():
         pwd = request.form.get("password")
         usr = User_Info.query.filter_by(email = uname,password = pwd).first()
         if usr and usr.role == 0 :
-            return render_template("admin_dashboard.html")
+            return redirect(url_for("admin_dashboard",name = uname))
         elif usr and usr.role == 1 :
-            return render_template("user_dashboard.html")
+            return redirect(url_for("user_dashboard",name = uname))
         else:
             return render_template("login.html",msg = "Invalid user credentials")
 
@@ -39,3 +39,35 @@ def signup():
         db.session.commit()
         return render_template("login.html",msg = "Registration Succesful, Login in now")
     return render_template("signup.html",msg = "")
+
+#Common route for admin dashboard
+@app.route("/admin/<name>")
+def admin_dashboard(name):
+    theatres=get_theatres()
+    return render_template("admin_dashboard.html",name=name,theatres=theatres)
+
+
+@app.route("/user/<name>")
+def user_dashboard(name):
+    return render_template("user_dashboard.html",name=name)
+
+
+@app.route("/venue/<name>",methods=["POST","GET"])
+def add_venue(name):
+    if request.method=="POST":
+        vname=request.form.get("name")
+        location=request.form.get("location")
+        pin_code=request.form.get("pin_code")
+        capacity=request.form.get("capacity")
+        new_theatre=Theatre(name=vname,location=location,pin_code=pin_code,capacity=capacity)
+        db.session.add(new_theatre)
+        db.session.commit()
+        return redirect(url_for("admin_dashboard",name=name))
+    
+    return render_template("add_venue.html",name=name)
+
+
+#Other supported functions
+def get_theatres():
+    theatres=Theatre.query.all()
+    return theatres
